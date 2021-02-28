e870c076f1df6bffe9a0f6705911a5c413f93d83
ab15963qw <36786222+ab15963qw@users.noreply.github.com>
2021-03-01 00:51:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
+  response.write("Hello World"); 
   response.end();
-}).listen(8888);
+}).listen(8888);//port 8888 可以修改port 位置，如果port有被佔用
